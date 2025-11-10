76773337c3fd9bea044e875246b51b62865c0c86
Satvik Gaur <satvik.gaur12@gmail.com>
2025-11-10 11:45:11 +0530

    Introduction to Java Script for the first time in BL_CW_10-11-2025

---
+++ Introduction.js
@@ -0,0 +1,32 @@
+// console.log("Hello, World!");
+// console.error("This is an error message.");
+// console.warn("This is a warning message.");
+// console.info("This is an informational message.");
+// console.debug("This is a debug message.");
+
+// console.table([{ name: "Alice", age: 30 }, { name: "Bob", age: 25 }]);
+// console.group("Grouped Messages");
+// console.log("Message 1 in group");
+// console.log("Message 2 in group");
+// console.groupEnd();
+// console.time("Timer");
+// for (let i = 0; i < 1000000; i++) {}
+// console.timeEnd("Timer");
+
+// console.trace("timer ended");                                                                                                      
+// console.clear();
+
+// let a = 5;
+// let b = 10;
+// console.log(`The sum of ${a} and ${b} is ${a + b}.`);
+
+// let user = { name: "Charlie", age: 28 };
+// console.log("User Details:", user);
+
+// let a = 1;
+// let b = "of the answer to life, the universe, and everything is";
+// let c = 69;
+// console.log("The value of a is:", a,b,c);
+
+// let array = { name: "Satvik", age: 21, role: "Developer" };
+// console.table(array);
